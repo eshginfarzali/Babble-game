daba8e9a2bb23e203ca943060f92773664432949
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-06-21 14:09:59 +0400

    feat: layout

---
+++ app/script.js
@@ -0,0 +1,11 @@
+const oneSection =document.getElementById("one-sec")
+const btnStartGameIndex = document.getElementById("btnStartGame")
+const input = document.getElementById("input")
+const gameBox =document.getElementById("gameBox")
+const gameMain =document.getElementById("game-main")
+
+
+btnStartGameIndex.addEventListener("click",()=>{
+
+    
+})
